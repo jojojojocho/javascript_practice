b654cdf3ba6f0a32abe8d500cb01eea3a4a313ee
jo <uuu9214@naver.com>
2022-05-08 22:04:15 +0900

    .

--- app.js
+++ app.js
@@ -1,9 +1,94 @@
-const player= {
-    name : "nico",
-    points : 10,
-    fat : true
-};
+// const player= {
+//     name : "cino",
+//     points : 10,
+//     fat : true
+// };
 
-console.log(player);
-console.log(player.name);
-player.name;
+// const list = ["mon", "sat"];
+// console.log(list);
+// list.push("sun");
+// console.log(list);
+// console.log(player);
+// console.log(player.name);
+// player.name;
+
+
+// -------------------------------------------------------------------------------
+
+
+// function sayHello(name){
+//     console.log("Hello my name is " + name);
+
+// }
+
+// sayHello("James");
+// sayHello("Andy");
+// sayHello("Maria");
+// sayHello("Jim");
+// sayHello("John");
+
+
+// const player = {
+//     name : "chino",
+//     sayHello : function(name){
+//         console.log("hello " + name);
+//     }
+// }
+
+
+// player.sayHello("lynn");
+
+
+
+// const five =5;
+// let isNicoFat =true;                                                                            
+// console.log(five); 
+
+
+// const calculator = {
+//     add: function(a,b){
+//         return a+b;
+//     },
+//     minus : function(a,b){
+//         return a-b;
+//     },
+//     divide : function(a,b){
+//         return a/b;
+//     },
+//     powerOf : function(a,b){
+//         return a**b;
+//     }
+
+// }
+
+// const result = calculator.add(10,2);
+// console.log(result);
+
+
+// const age = parseInt(prompt('How old are u?'));
+
+// if(isNaN(age) || age < 0){
+//     console.log("Please write a real positive number");
+// }else if (age<18){
+//     console.log("You are too young.");
+// }else if(age>=18 && age<=50){
+//     console.log("you can drink.!");
+// }else if(age >50 && age<=80){
+//     console.log("You should excercise!");
+// }else if(age === 100){
+//     console.log("You are wise");
+// }else  if(age > 80 ){
+//     console.log("You can do whatever you want!");
+// }
+
+// const title = document.getElementById("title");
+// title.innerText = "Got you!";
+// console.dir(title.id);
+// console.dir(title.className);
+
+
+const title = document.querySelector(".hello h1")
+
+console.log(title);
+
+
